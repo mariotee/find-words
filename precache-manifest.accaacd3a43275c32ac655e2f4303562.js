602b23afbf0892f5ac1b9ed82cfc89a6b082397b
mariotee <mariotorresdev@gmail.com>
2020-10-06 23:39:58 -0500

    Updates

---
+++ precache-manifest.accaacd3a43275c32ac655e2f4303562.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c1c51c229bbc2d6f65b77b6f80172b7",
+    "url": "/find-words/index.html"
+  },
+  {
+    "revision": "3a2f1e6e4473c16d1b4d",
+    "url": "/find-words/static/css/main.90b94a5a.chunk.css"
+  },
+  {
+    "revision": "a44586dbb83369053a5f",
+    "url": "/find-words/static/js/2.875fe73b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/find-words/static/js/2.875fe73b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a2f1e6e4473c16d1b4d",
+    "url": "/find-words/static/js/main.efec1a82.chunk.js"
+  },
+  {
+    "revision": "137e6dee670d513969e8",
+    "url": "/find-words/static/js/runtime-main.c2d620d6.js"
+  }
+]);
