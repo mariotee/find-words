9305c737cb89f843085668fa66110d7255ed6a5a
mariotee <mariotorresdev@gmail.com>
2020-10-07 14:14:31 -0500

    Updates

---
+++ precache-manifest.da1108c7fab9cf14b9ea02eec3800229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f184d1f29672a29bafdd12a0b5aee75",
+    "url": "/find-words/index.html"
+  },
+  {
+    "revision": "83968efc30e146f200b5",
+    "url": "/find-words/static/css/main.c17015b5.chunk.css"
+  },
+  {
+    "revision": "2a84f7d5792e6101b2fa",
+    "url": "/find-words/static/js/2.5c769917.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/find-words/static/js/2.5c769917.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83968efc30e146f200b5",
+    "url": "/find-words/static/js/main.e21a40ca.chunk.js"
+  },
+  {
+    "revision": "de7386614bac752d0388",
+    "url": "/find-words/static/js/runtime-main.e201e53e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/find-words/precache-manifest.f868d37915a45e02a14ca849f2cb2013.js"
+  "/find-words/precache-manifest.da1108c7fab9cf14b9ea02eec3800229.js"
 );
 
 self.addEventListener('message', (event) => {
